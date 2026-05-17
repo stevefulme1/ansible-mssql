319d857e93f13abefa1eddca2ffbd06ea42ca117
sfulmer <sfulmer@redhat.com>
2026-05-17 09:52:57 -0400

    fix: resolve CI failures

--- tests/unit/plugins/modules/test_mssql_user.py
+++ tests/unit/plugins/modules/test_mssql_user.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 import pytest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 
 from ansible_collections.stevefulme1.mssql.plugins.modules import mssql_user
 
